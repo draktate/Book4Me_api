80d52d6064e0bfa674410f3829d272b6ff87894b
draktate <draktate@gmail.com>
2023-03-01 18:21:14 +0530

    changes for adding origins

--- app.js
+++ app.js
@@ -42,14 +42,32 @@ const connect = async() =>{
 }
 
 const app = express();
-/*
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-*/
+
+    //credentials:true,            
+    //access-control-allow-credentials:true
+    //access-control-allow-origin:*,
+//const allowedOrigins = ['http://localhost:3000','http://yourapp.com'];
+const allowedOrigins = process.env.ORIGINs.split(','); 
+
+
+app.use(cors({
+        origin: function(origin, callback){
+          // allow requests with no origin 
+          // (like mobile apps or curl requests)
+          if(!origin) return callback(null, true);
+          if(allowedOrigins.indexOf(origin) === -1){
+            var msg = 'The CORS policy for this site does not ' +
+                      'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+          }
+          return callback(null, true);
+        }
+      }));
+
+
+      console.log("allowed from origins: ",allowedOrigins )
+//    app.use(cors(corsOptions));
+
 
 app.use(cookieParser());
 app.use(express.json());
